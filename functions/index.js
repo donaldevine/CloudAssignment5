10e59aa102942fd99a2856565964dc3510a935f3
Donal Devine <5518040+donaldevine@users.noreply.github.com>
2023-11-02 20:53:02 +0000

    initial checkin

---
+++ functions/index.js
@@ -0,0 +1,41 @@
+/**
+ * Import function triggers from their respective submodules:
+ *
+ * const {onCall} = require("firebase-functions/v2/https");
+ * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
+ *
+ * See a full list of supported triggers at https://firebase.google.com/docs/functions
+ */
+
+const {onRequest} = require("firebase-functions/v2/https");
+const logger = require("firebase-functions/logger");
+const {onDocumentCreated} = require("firebase-functions/v2/firestore");
+const {initializeApp} = require("firebase-admin/app");
+const {getFirestore} = require("firebase-admin/firestore");
+
+initializeApp();
+
+exports.getcomments = onRequest(async (request, response) => {
+    logger.info("getcomments called", {structuredData: true});
+    const db = getFirestore();
+    const comments = await db.collection("comments").get();
+    const commentsData = comments.docs.map((doc) => doc.data());
+    response.send(commentsData);
+});
+
+exports.postcomment = onRequest(async (request, response) => {
+    logger.info("postcomment called", {structuredData: true});
+    const db = getFirestore();
+    const {name, comment} = request.body;
+    await db.collection("comments").add({name, comment});
+    response.send("Comment added");
+});
+
+
+// Create and deploy your first functions
+// https://firebase.google.com/docs/functions/get-started
+
+// exports.helloWorld = onRequest((request, response) => {
+//   logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
---
+++ public/script/script.js
@@ -0,0 +1,35 @@
+async function getComments() {
+    const response = await fetch("https://us-central1-assignment5-23205918.cloudfunctions.net/getcomments");
+    const movies = await response.json();
+    console.log(movies);
+}
+
+async function postComment(data) {
+    try {
+        const response = await fetch("https://us-central1-assignment5-23205918.cloudfunctions.net/postcomment", {
+            method: "POST", // or 'PUT'
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data),
+        });
+
+        const result = await response.json();
+        console.log("Success:", result);
+    } catch (error) {
+        console.error("Error:", error);
+    }
+}
+
+
+var commentsForm = document.getElementById("commentsForm");
+
+commentsForm.addEventListener("submit", (e) => {
+   e.preventDefault();
+
+   let comment = document.getElementById("comment");
+
+   if (comment.value){
+       postComment(comment);
+   }
+});
